492d0f943b7ad43ee45cb9baccceb4fc8f038dbb
kaka17 <841623088@qq.com>
2017-02-28 13:51:56 +0800

    login

--- app/src/main/java/com/kaka/smargame/ui/activity/LoginActivity.java
+++ app/src/main/java/com/kaka/smargame/ui/activity/LoginActivity.java
@@ -11,9 +11,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.kaka.smargame.AppContext;
+import com.kaka.smargame.Config;
 import com.kaka.smargame.R;
+import com.kaka.smargame.api.base.HttpCallBack;
+import com.kaka.smargame.api.base.HttpClient;
+import com.kaka.smargame.api.base.ResponseBean;
 import com.kaka.smargame.ui.MainActivity;
 import com.kaka.smargame.ui.activity.base.BaseActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends BaseActivity implements View.OnClickListener{
 
@@ -27,6 +35,7 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
 //        setContentView(R.layout.activity_login);
         initView();
         setOnClistener();
+
     }
 
     @Override
@@ -44,26 +53,48 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
         etPwd = (EditText) findViewById(R.id.etPwd);
         btLogin = (Button) findViewById(R.id.btLogin);
         tvRegist = (TextView) findViewById(R.id.tvRegist);
-
     }
     private void setOnClistener(){
         btLogin.setOnClickListener(this);
         tvRegist.setOnClickListener(this);
-
-
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btLogin:
-                Intent intent=new Intent(getApplicationContext(), MainGameActivity.class);
-                startActivity(intent);
+//                Intent intent=new Intent(getApplicationContext(), MainGameActivity.class);
+//                startActivity(intent);
+                String ureName = etName.getText().toString().trim();
+                String pwd = etPwd.getText().toString().trim();
+                if (ureName.equals("")||ureName.isEmpty()||pwd.equals("")||pwd.isEmpty()){
+                    AppContext.toast("请输入账号密码");
+                    return;
+                }
+                login(ureName,pwd );
                 break;
             case R.id.tvRegist:
                 startActivity(new Intent(getApplicationContext(),RegistActivity.class));
                 break;
         }
+    }
+    private void login(String ureName,String pwd){
+        HttpClient.instance().login02(ureName, pwd, new HttpCallBack() {
+            @Override
+            public void onSuccess(ResponseBean responseBean) {
+
+                try {
+                    JSONObject object=new JSONObject(responseBean.toString());
+                    if (object.getString(Config.code).equals("0000")){
+                        AppContext.toast(object.getString(Config.msg));
+                    }else {
+                        AppContext.toast(responseBean.toString());
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
